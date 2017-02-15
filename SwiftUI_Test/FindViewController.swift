62ade89352b3def1179c0c2f92dc208554ab10c3
TTanys <767772098@qq.com>
2017-02-15 18:33:10 +0800

    改动

--- SwiftUI_Test/FindViewController.swift
+++ SwiftUI_Test/FindViewController.swift
@@ -14,8 +14,27 @@ class FindViewController: UIViewController {
         super.viewDidLoad()
         
         self.title = "发现"
+        
+        
+        
+        
+        var a = 20 ,b = 5
+        SumAndMinus(num1: a, num2: b, sum: &a, minus: &b)
+        
+        SumAndMinus(num1: a, num2: b, sum: &a, minus: &b)
+        
+//        print("\(SumAndMinus(num1: a, num2: b, sum: &a, minus: &b))")
+        
     }
 
-    
+    func SumAndMinus(num1:Int,num2:Int, sum:inout Int, minus:inout Int){
+        
+        sum = num1 + num2
+        
+        minus = num1 - num2
+        
+        print("\(sum)   \(minus)")
+        
+    }
 
 }
